886db1b35ddd847fe700a6c01d5c12d406f39cc3
Jacob Celestine <jacobceles@gmail.com>
2021-12-04 21:12:32 -0500

    Squashed some pesky bugs

--- ui/app.py
+++ ui/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 w = 3
 h = 3
 state = Board(3, 3)
+player_names = []
 current_player = "player1"
 players_details = dict()
 # basic colors
@@ -30,25 +31,25 @@ def make_board_state(new_state):
     for item in new_state:
         if item[0].isdigit() or item.startswith('-'):
             col += 1
-            if item[3:] == 1:
+            if item[3:] == "1":
                 color = colors[0]
-            elif item[3:] == 2:
+            elif item[3:] == "2":
                 color = colors[1]
-            elif item[3:] == 3:
+            elif item[3:] == "3":
                 color = colors[2]
-            elif item[3:] == 4:
+            elif item[3:] == "4":
                 color = colors[3]
-            elif item[3:] == 12:
+            elif item[3:] == "12":
                 color = colors[4]
-            elif item[3:] == 34:
+            elif item[3:] == "34":
                 color = colors[5]
-            elif item[3:] == 13:
+            elif item[3:] == "13":
                 color = colors[6]
-            elif item[3:] == 24:
+            elif item[3:] == "24":
                 color = colors[7]
-            elif item[3:] == 14:
+            elif item[3:] == "14":
                 color = colors[8]
-            elif item[3:] == 23:
+            elif item[3:] == "23":
                 color = colors[9]
             else:
                 color = colors[10]
@@ -66,13 +67,14 @@ def next_state():
     :accepts move: Move made by player. Example: 1,2
     :return: JSON with game status, winner name and new board state
     """
-    global players_details, state, current_player, w, h, colors
-    move = tuple(map(int, request.args.get('move').split(",")))
+    global player_names, players_details, state, current_player, w, h, colors
+    move = tuple(map(int, request.args.get('move', "0,0").split(",")))
 
-    if current_player != "player4":
+    if current_player != player_names[3]:
         _, move = minimax_step(state.get_copy(), deepcopy(players_details[current_player]), 0)
     print(f'{current_player} placing on {move}')
     state, player, utilities, terminal = game_step(state, players_details[current_player], move)
+    current_player = player_names[int(str(player.name)[-1])-1]
     print("New State:")
     print(format_board(state))
     new_state = str(str(format_board(state))[1:-1].split(" ")).split("'")
@@ -102,7 +104,7 @@ def play():
     :accepts players: A comma separated string of player names
     :return: JSON with game status, next player name and new board state
     """
-    global players_details, state, current_player, w, h
+    global player_names, players_details, state, current_player, w, h
 
     w = int(request.args.get('w'))
     h = int(request.args.get('h'))
